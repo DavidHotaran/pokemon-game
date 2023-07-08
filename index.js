0bd3c9701711876b425dc49b1d6e801b4ce87191
DavidHotaran <hotaran.d98@gmail.com>
2023-07-07 20:00:56 -0600

    add requestAnimationFrame & simplify logic

--- index.js
+++ index.js
@@ -3,71 +3,76 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 const ctx = canvas.getContext("2d");
 const image = new Image();
-let X = innerWidth / 2;
-let Y = innerHeight / 2;
 const IMG_X = 35;
 const IMG_Y = 35;
-let moveSpeed = 10;
 const ALLOWED_KEYS = ['w', 'a', 's', 'd', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+let X = innerWidth / 2;
+let Y = innerHeight / 2;
+let move = false;
+let moveSpeed = 10;
 
 image.src = '../assets/down.png';
 image.onload = () => {
     ctx.drawImage(image, X, Y, IMG_X, IMG_Y); // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
-};
+}
 
 window.addEventListener('keydown', (e) => {
-    console.log(X, Y)
     if (!ALLOWED_KEYS.includes(e.key)) {
         return;
     };
 
-    ctx.clearRect(0, 0, innerWidth, innerHeight)
-
+    // console.log(X, Y)
     switch (e.key) {
         case 'w':
         case 'ArrowUp':
+            image.src = '../assets/up.png';
             if (Y < 10) { //at top of screen
-                image.src = '../assets/up.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = false;
             } else {
                 Y -= moveSpeed;
-                image.src = '../assets/up.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = true;
             }
-            return;
+            break;
         case 'a':
         case 'ArrowLeft':
+            image.src = '../assets/left.png';
             if (X < 10) { // at right of screen
-                image.src = '../assets/left.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = false;
             } else {
                 X -= moveSpeed;
-                image.src = '../assets/left.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = true;
             }
-            return;
+            break;
         case 's':
         case 'ArrowDown':
+            image.src = '../assets/down.png';
             if (Y >= innerHeight - IMG_Y) {
-                console.log("hre 731")
-                image.src = '../assets/down.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = false;
             } else {
                 Y += moveSpeed;
-                image.src = '../assets/down.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = true;
             }
-            return;
+            break;
         case 'd':
         case 'ArrowRight':
+            image.src = '../assets/right.png';
             if (X > innerWidth - IMG_X) {
-                image.src = '../assets/right.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = false;
             } else {
                 X += moveSpeed;
-                image.src = '../assets/right.png';
-                ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+                move = true;
             }
-            return;
+            break;
     };
 });
+
+
+function game() {
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    ctx.drawImage(image, X, Y, IMG_X, IMG_Y);
+
+    requestAnimationFrame(game);
+};
+
+
+game();
